37088078a8ba2bcc5441889c4411d55a27ee0514
GrantDvo <168016702+GrantDvo@users.noreply.github.com>
2025-10-26 16:10:33 -0500

    Fixed API URL and login endpoint

--- src/http/APIService.js
+++ src/http/APIService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Change the API_URL to the correct location of the backend API before deploying the app
-const API_URL = 'https://gdvorak.pythonanywhere.com/';
+const API_URL = 'https://gdvorak.pythonanywhere.com';
 /* 'http://localhost:8000' http://127.0.0.1:8000/
 or 'http://yourPythonAnywhereName.pythonanywhere.com/' */
 
@@ -117,7 +117,7 @@ export class APIService {
   }
 
   authenticateLogin(credentials) {
-    const url = `${API_URL}/api/`;
+    const url = `${API_URL}/api/token/`;
     return axios.post(url, credentials);
   }
 
